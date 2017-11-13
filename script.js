953f7a44e74234c4d6d315008a9013b77826722b
Kaique Lupo Leite <kaique.leite@usp.br>
2017-11-13 21:17:38 -0200

    script

--- script.js
+++ script.js
@@ -9,15 +9,27 @@ $('#selection').on('change', function() {
   show(value);
 });
 
+function isPoint(value){
+	for(i = 0; i < selected_points.length; i++){
+		if(parseInt(value) == parseInt(selected_points[i])) return true;
+	}
+
+	return false;
+}
+
 function coordinates(){
 	scatterplot_dataset(parseInt($("#selection_scatterplot_1").val()), 
 			parseInt($("#selection_scatterplot_2").val()));
 }
 
 function draw_points(svg){
-	svg.selectAll(".dot").style("fill", function(d) {
-		return "#CCC";
-	});
+	if(selected_points.length > 0){
+		var color = d3.scale.linear().domain([1, 156]).range(["brown", "steelblue"]);
+
+		svg.selectAll(".dot").style("fill", function(d) {
+			return (isPoint(d.id)) ? color(d.class) : "#CCC";
+		});
+	}
 }
 
 function show(idx){
@@ -26,11 +38,16 @@ function show(idx){
 	else $("#menu-bottom").hide();
 
 
-	if(idx == 0 && document.getElementById("scatterplot-pca").innerHTML == "") svgs[0] = scatterplot();
-	else draw_points(svgs[0]);
+	if(idx == 0){
+		if(document.getElementById("scatterplot-pca").innerHTML == "") svgs[0] = scatterplot();
+	}else draw_points(svgs[0]);
+
   	if(idx == 1 && document.getElementById("parallel-coordinates").innerHTML == "") parallel_coordinates();
-  	if(idx == 2 && document.getElementById("scatterplot-dataset").innerHTML == "") svgs[1] = scatterplot_dataset();
-  	else draw_points(svgs[1]);
+
+  	if(idx == 2){
+  		if(document.getElementById("scatterplot-dataset").innerHTML == "") svgs[1] = scatterplot_dataset();
+  	}else draw_points(svgs[1]);
+
   	if(idx == 3 && document.getElementById("table").innerHTML == "") table();
 
 
